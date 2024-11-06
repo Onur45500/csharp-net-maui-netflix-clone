cbed500f4c1724ddd97f23d1e6a241941dfa9ae0
onur.akmese <onur.akmese@viacesi.fr>
2024-11-06 11:13:46 +0100

    Created the the movie db service

---
+++ csharp-net-maui-netflix-clone/Services/TmdbService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_net_maui_netflix_clone.Services
+{
+    public class TmdbService
+    {
+        private const string ApiKey = "";
+        public const string TmdbHttpClientName = "TmdbCleint";
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TmdbService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private HttpClient HttpClient => _httpClientFactory.CreateClient(TmdbHttpClientName)
+    }
+}
