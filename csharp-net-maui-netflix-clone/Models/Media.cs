9b163f44a9dc20c258b712f1ba77bbccf9f4416b
onura <onur.akmese@viacesi.fr>
2024-11-08 23:45:48 +0100

    Modified the media model

--- csharp-net-maui-netflix-clone/Models/Media.cs
+++ csharp-net-maui-netflix-clone/Models/Media.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace csharp_net_maui_netflix_clone.Models
 {
-    internal class Media
+    public class Media
     {
+        public string backdrop_path { get; set; }
+        public int[] genre_ids { get; set; }
+        public int id { get; set; }
+        public string original_title { get; set; }
+        public string original_name { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+        public string release_date { get; set; }
+        public string title { get; set; }
+        public string name { get; set; }
+        public bool video { get; set; }
+        public string media_type { get; set; } // "movie" or "tv"
+        public string ThumbnailPath => poster_path ?? backdrop_path;
+        public string Thumbnail => $"https://image.tmdb.org/t/p/w600_and_h900_bestv2/{ThumbnailPath}";
+        public string ThumbnailSmall => $"https://image.tmdb.org/t/p/w220_and_h330_face/{ThumbnailPath}";
+        public string ThumbnailUrl => $"https://image.tmdb.org/t/p/original/{ThumbnailPath}";
+        public string DisplayTitle => title ?? name ?? original_title ?? original_name;
     }
 }
