ce0b042ac2dea94bc4a9ea42b78e98acdaeb12d3
onura <onur.akmese@viacesi.fr>
2024-11-10 13:57:18 +0100

    Initiliazed the hom view model with the trending movies

--- csharp-net-maui-netflix-clone/ViewModels/HomeViewModel.cs
+++ csharp-net-maui-netflix-clone/ViewModels/HomeViewModel.cs
@@ -1,8 +1,36 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using csharp_net_maui_netflix_clone.Models;
+using csharp_net_maui_netflix_clone.Services;
+using System.Collections.ObjectModel;
 
 namespace csharp_net_maui_netflix_clone.ViewModels
 {
     public partial class HomeViewModel : ObservableObject
     {
+        TmdbService _tmdbService;
+
+        public HomeViewModel(TmdbService tmdbService)
+        {
+            _tmdbService = tmdbService;
+        }
+        [ObservableProperty]
+        private Media trendingMedia;
+
+        public ObservableCollection<Media> Trending { get; set; } = new();
+        public ObservableCollection<Media> TopRated { get; set; } = new();
+        public ObservableCollection<Media> NetflixOriginals { get; set; } = new();
+        public ObservableCollection<Media> ActionMovies { get; set; } = new();
+
+        public async Task InitializeAsync()
+        {
+            var trendingList = await _tmdbService.GetTrendingAsync();
+            if(trendingList?.Any() == true)
+            {
+                foreach(var trending in trendingList)
+                {
+                    Trending.Add(trending);
+                }
+            }
+        }
     }
 }
